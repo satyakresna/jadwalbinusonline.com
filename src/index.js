70b4440c76e70051ba2dcd5b0e82bfdcd74da01d
Kresna <satyakresna6295@gmail.com>
2020-11-26 22:53:25 +0800

    feat: mechanism to store new schedule

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
+const pkgDir = require('pkg-dir');
+const { join } = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
 const getSchedule = require('./scraper/schedule.js');
 
 (async () => {
-    const result = await getSchedule();
+    try {
+        const newSchedule = await getSchedule();
+        const rootDir = await pkgDir(__dirname);
+        const outputFile = join(rootDir, 'data', 'schedules.json');
+        const text = await fsPromises.readFile(outputFile, 'utf8');
+        // Jika isi teks kosong maka ini adalah sebuah inputan baru.
+        if (text === "") {
+            let newSchedules = [];
+            newSchedules.push(newSchedule);
+            fs.writeFile(outputFile, JSON.stringify(newSchedules), err => {
+                if (err) {
+                    console.log(err);
+                }
 
-    // TODO: Store result to file
-    // Array of objects
-    console.log(result);
+                console.log('Jadwal baru berhasil disimpan');
+            });
+        } else {
+            // Ambil object paling akhir di dalam array
+            // Bandingkan object paling akhir itu dengan object yang diambil dari scraper
+            // Gunakan key "batas_pendaftaran" sebagai pembanding satu sama lain.
+            let schedules = JSON.parse(text);
+            const lastSchedule = schedules[schedules.length - 1];
+            if (lastSchedule.batas_pendaftaran !== newSchedule.batas_pendaftaran) {
+                schedules.push(newSchedule);
+                fs.writeFile(outputFile, JSON.stringify(schedules), err => {
+                    if (err) {
+                        console.log(err);
+                    }
+    
+                    console.log('Jadwal baru berhasil disimpan');
+                });
+            } else {
+                console.log("Tidak ada jadwal baru");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    } 
 })();
