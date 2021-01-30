5c7fe959dac24293b770b4cef81e47fed7237e8a
Kresna <satyakresna6295@gmail.com>
2021-01-30 15:21:47 +0800

    improve site
    1. Add appresiasi section
    2. Add background to alert
    3. Set border red when field is empty
    4. Set Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [
     'frontend/**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   variants: {
     extend: {},
