a0dbe9e10787a1f8938d58f63214d2fcb2cf70c0
Kresna <satyakresna6295@gmail.com>
2021-01-30 15:50:05 +0800

    fix analytics config

--- analytics.js
+++ analytics.js
@@ -1,20 +1,18 @@
-require('dotenv').config();
+// require('dotenv').config();
 const path = require('path');
 const fs = require('fs');
 
 let result = '';
 const search = '</head>';
-const GA_TRACKER_ID = process.env.GA_TRACKER_ID;
-const str = `<link rel="preconnect" href="https://www.google-analytics.com">
-<!-- Google Analytics -->
-<script defer>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.defer=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    
-    ga('create', '${GA_TRACKER_ID}', 'auto');
-    ga('send', 'pageview');
+// const GA_TRACKER_ID = process.env.GA_TRACKER_ID;
+const str = `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S023R4K9E6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S023R4K9E6');
 </script>`;
 
 var readStream = fs.createReadStream(path.join(__dirname, 'frontend', 'index.html'), 'utf8');
@@ -25,11 +23,12 @@ readStream.on('data', function (chunk) {
     if (index > -1) {
         start = chunk.substr(0, index);
         end = chunk.substr(index);
-        if (GA_TRACKER_ID !== undefined) {
-            result += start + str + end;
-        } else {
-            result += chunk;
-        }
+        result += start + str + end;
+        // if (GA_TRACKER_ID !== undefined) {
+            
+        // } else {
+        //     result += chunk;
+        // }
     } else {
         result += chunk;
     }
